2aebd2073b7e43602ce2313947b2dc3f1c5138a9
Gabo <gabriel.piles.glez@gmail.com>
2024-09-30 20:21:24 +0200

    Implement ml cloud connector execute on queue processor

--- src/start_queue_processor.py
+++ src/start_queue_processor.py
@@ -1,15 +1,45 @@
 import os
+
+from ml_cloud_connector.MlCloudConnector import MlCloudConnector
 from pydantic_core._pydantic_core import ValidationError
 from queue_processor.QueueProcessor import QueueProcessor
 
 from configuration import service_logger, REDIS_HOST, REDIS_PORT, QUEUES_NAMES
 from data_model.Translation import Translation
 from data_model.TranslationResponseMessage import TranslationResponseMessage
+from data_model.TranslationTask import TranslationTask
 from data_model.TranslationTaskMessage import TranslationTaskMessage
 from translate import get_translation
 
 from sentry_sdk.integrations.redis import RedisIntegration
 import sentry_sdk
+
+
+def get_empty_translation(translation_task):
+    return Translation(
+        text="",
+        language=translation_task.language_to,
+        success=True,
+        error_message="",
+    )
+
+
+def get_error_translation(translation_task: TranslationTask, error_message: str):
+    return Translation(
+        text=translation_task.text,
+        language=translation_task.language_to,
+        success=False,
+        error_message=error_message,
+    )
+
+
+def get_translation_from_task(translation_task: TranslationTask):
+    if not translation_task.text.strip():
+        return get_empty_translation(translation_task)
+
+    connector = MlCloudConnector("translation")
+    translation, finished, error = connector.execute(get_translation, service_logger, translation_task)
+    return translation if finished else get_error_translation(translation_task, error)
 
 
 def process(message):
@@ -20,30 +50,10 @@ def process(message):
         service_logger.error(f"Not a valid Redis message: {message}")
         return None
 
-    try:
-        translations = [get_translation(translation_task) for translation_task in task_message.get_tasks()]
-    except BrokenPipeError:
-        response = TranslationResponseMessage(
-            **task_message.model_dump(),
-            translations=[
-                Translation(
-                    text=translation.text,
-                    language=translation.language_to,
-                    success=False,
-                    error_message="Server unavailable",
-                )
-                for translation in task_message.get_tasks()
-            ],
-        )
-
-        return response.model_dump()
-
-    response = TranslationResponseMessage(
+    return TranslationResponseMessage(
         **task_message.model_dump(),
-        translations=translations,
-    )
-
-    return response.model_dump()
+        translations=[get_translation_from_task(task_message) for task_message in task_message.get_tasks()],
+    ).model_dump()
 
 
 if __name__ == "__main__":
