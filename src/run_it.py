18e4d4546fd2ae4aa82aee8897191f0927adbf3b
Gabo <gabriel.piles.glez@gmail.com>
2025-05-02 17:37:11 +0200

    Format code

--- src/run_it.py
+++ src/run_it.py
@@ -8,7 +8,9 @@ def run_it():
     start = time()
     print("start")
     text = "El juez ordena arresto domiciliar, ligarlo a proceso y una caución económica de Q10,000."
-    translation_task_message = TranslationTaskMessage(key="key", text=text, language_from="Spanish", languages_to=["English"])
+    translation_task_message = TranslationTaskMessage(
+        key="key", text=text, language_from="Spanish", languages_to=["English"]
+    )
     results = process(translation_task_message.model_dump())
     translation_response_message = TranslationResponseMessage(**results)
     print(translation_response_message.model_dump())
